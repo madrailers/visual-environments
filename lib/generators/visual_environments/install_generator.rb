c5666b37288c67672cc984b3f425ae7c8da1a1cf
Zachery Moneypenny <whazzmaster@gmail.com>
2011-02-20 14:28:29 -0600

    Added a generator to create the initializer file, documented the available settings int he README, added functionality to switch the side of the banner (upper-left vs upper-right corner), and added ability to alias environment names.  See README for updated functionality details and examples.

---
+++ lib/generators/visual_environments/install_generator.rb
@@ -0,0 +1,30 @@
+require 'rails/generators/base'
+
+module VisualEnvironments
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+    
+      def copy_initializer_file
+        copy_file File.join(File.dir(__FILE__), "templates/initializer.rb"), "config/initializers/visual-environment.rb"
+      end
+
+      def self.banner
+        "rails generate visual-environments:install"
+      end
+
+      private
+
+      def add_gem(name, options = {})
+        gemfile_content = File.read(destination_path("Gemfile"))
+        File.open(destination_path("Gemfile"), 'a') { |f| f.write("\n") } unless gemfile_content =~ /\n\Z/
+        gem name, options unless gemfile_content.include? name
+      end
+
+      def print_usage
+        self.class.help(Thor::Base.shell.new)
+        exit
+      end
+  
+    end
+  end
+end
---
+++ lib/generators/visual_environments/templates/initializer.rb
@@ -0,0 +1,6 @@
+ActionController::Base::VisualEnvironments.enable_env_in_title = true
+# By default, the corner banner is disabled
+ActionController::Base::VisualEnvironments.enable_env_in_corner_banner = false
+# By default, the visual-environments will be enabled for all environments except production;
+# to exclude additional environments, uncomment the line below and add them to the collection
+# ActionController::Base::VisualEnvironments.excluded_environments = []
