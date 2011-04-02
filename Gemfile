49b93d36e07ab794e3a150feb8142061c4688240
Zachery Moneypenny <whazzmaster@gmail.com>
2011-04-01 20:06:14 -0500

    Slight refactoring to make the reopening of ActionController::Base as simple as possible

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source "http://rubygems.org"
 # Add dependencies required to use your gem here.
-# Example:
-#   gem "activesupport", ">= 2.3.5"
-gem "rails"
+
+gem 'rails', '>= 3.0.5'
+gem 'activesupport', '>= 3.0.5'
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
-group :development do
+group :development, :test do
 	gem "rspec-rails"
   gem "bundler", "~> 1.0.0"
   gem "jeweler", "~> 1.5.2"
--- lib/visual-environments.rb
+++ lib/visual-environments.rb
@@ -36,40 +36,39 @@ module VisualEnvironments
    @@title_aliases.replace aliases
    @@corner_banner_aliases.replace aliases
  end
+ 
+ def shim_visual_environments
+   unless Rails.env == 'production' || (VisualEnvironments.excluded_environments && VisualEnvironments.excluded_environments.include?(Rails.env))
+     
+     if VisualEnvironments.enable_env_in_title
+       current_env = Rails.env
+       current_env = VisualEnvironments.title_aliases[current_env] if !VisualEnvironments.title_aliases.nil? && VisualEnvironments.title_aliases.has_key?(current_env)
+       shim = "<script type='text/javascript'>var te=document.getElementsByTagName('title')[0];var t=te.innerHTML;te.innerHTML=\"["+current_env+"] \"+t;</script></body>"
+       response.body = response.body.gsub /\<\/body\>/, shim
+     end
+   
+     unless VisualEnvironments.corner_banner_side.nil?
+       # Exit if we detect invalid options in the corner_banner_side option
+       return unless [:left,:right].include?(VisualEnvironments.corner_banner_side)
+       current_env = Rails.env
+       current_env = VisualEnvironments.corner_banner_aliases[current_env] if !VisualEnvironments.corner_banner_aliases.nil? && VisualEnvironments.corner_banner_aliases.has_key?(current_env)
+       shim = "<script type='text/javascript'>var con=document.createElement('div');con.id='VisualEnvironment_container';document.body.appendChild(con);var ce=document.createElement('div');ce.id='VisualEnvironment_corner';ce.innerHTML='"+current_env+"';con.appendChild(ce);</script>"
+       
+       if VisualEnvironments.corner_banner_side == :right
+         shim += "<style>#VisualEnvironment_corner{position:absolute;top:20px;right:-35px;font-family:Verdana;font-size:10px;color:#fff;padding:9px;width:120px;text-align:center;background:#000;border:#fff 1px solid;filter:alpha(opacity=70);-moz-opacity:0.7;-khtml-opacity:0.7;opacity:0.7;-webkit-transform:rotate(45deg);-moz-transform:rotate(45deg);}#VisualEnvironment_container{position:absolute;top:0;right:0;width:120px;height:93px;overflow:hidden;}</style>"
+       elsif VisualEnvironments.corner_banner_side == :left
+         shim += "<style>#VisualEnvironment_corner{position:absolute;top:20px;left:-35px;font-family:Verdana;font-size:10px;color:#fff;padding:9px;width:120px;text-align:center;background:#000;border:#fff 1px solid;filter:alpha(opacity=70);-moz-opacity:0.7;-khtml-opacity:0.7;opacity:0.7;-webkit-transform:rotate(-45deg);-moz-transform:rotate(-45deg);}#VisualEnvironment_container{position:absolute;top:0;left:0;width:120px;height:93px;overflow:hidden;}</style>"
+       end
+       shim += "</body>"
+       response.body = response.body.gsub /\<\/body\>/, shim
+     end  
+     
+   end
+ end
 
 end
 
 class ActionController::Base
   include VisualEnvironments
   after_filter :shim_visual_environments  
-  
-  def shim_visual_environments
-    unless Rails.env == 'production' || (VisualEnvironments.excluded_environments && VisualEnvironments.excluded_environments.include?(Rails.env))
-      
-      if VisualEnvironments.enable_env_in_title
-        current_env = Rails.env
-        current_env = VisualEnvironments.title_aliases[current_env] if !VisualEnvironments.title_aliases.nil? && VisualEnvironments.title_aliases.has_key?(current_env)
-        shim = "<script type='text/javascript'>var te=document.getElementsByTagName('title')[0];var t=te.innerHTML;te.innerHTML=\"["+current_env+"] \"+t;</script></body>"
-        response.body = response.body.gsub /\<\/body\>/, shim
-      end
-    
-      unless VisualEnvironments.corner_banner_side.nil?
-        # Exit if we detect invalid options in the corner_banner_side option
-        return unless [:left,:right].include?(VisualEnvironments.corner_banner_side)
-        current_env = Rails.env
-        current_env = VisualEnvironments.corner_banner_aliases[current_env] if !VisualEnvironments.corner_banner_aliases.nil? && VisualEnvironments.corner_banner_aliases.has_key?(current_env)
-        shim = "<script type='text/javascript'>var con=document.createElement('div');con.id='VisualEnvironment_container';document.body.appendChild(con);var ce=document.createElement('div');ce.id='VisualEnvironment_corner';ce.innerHTML='"+current_env+"';con.appendChild(ce);</script>"
-        
-        if VisualEnvironments.corner_banner_side == :right
-          shim += "<style>#VisualEnvironment_corner{position:absolute;top:20px;right:-35px;font-family:Verdana;font-size:10px;color:#fff;padding:9px;width:120px;text-align:center;background:#000;border:#fff 1px solid;filter:alpha(opacity=70);-moz-opacity:0.7;-khtml-opacity:0.7;opacity:0.7;-webkit-transform:rotate(45deg);-moz-transform:rotate(45deg);}#VisualEnvironment_container{position:absolute;top:0;right:0;width:120px;height:93px;overflow:hidden;}</style>"
-        elsif VisualEnvironments.corner_banner_side == :left
-          shim += "<style>#VisualEnvironment_corner{position:absolute;top:20px;left:-35px;font-family:Verdana;font-size:10px;color:#fff;padding:9px;width:120px;text-align:center;background:#000;border:#fff 1px solid;filter:alpha(opacity=70);-moz-opacity:0.7;-khtml-opacity:0.7;opacity:0.7;-webkit-transform:rotate(-45deg);-moz-transform:rotate(-45deg);}#VisualEnvironment_container{position:absolute;top:0;left:0;width:120px;height:93px;overflow:hidden;}</style>"
-        end
-        shim += "</body>"
-        response.body = response.body.gsub /\<\/body\>/, shim
-      end  
-      
-    end
-  end
-  
 end
